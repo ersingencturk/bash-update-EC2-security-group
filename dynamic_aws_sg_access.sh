f44759c289c24452ebc9ab12366e7091ef3c2554
Ersin <ersingencturk@gmail.com>
2019-05-05 03:47:17 +0300

    Update dynamic_aws_sg_access.sh

--- dynamic_aws_sg_access.sh
+++ dynamic_aws_sg_access.sh
@@ -18,8 +18,10 @@ function error_and_die() {
 # Declare variables because autism.
 declare access_granted="false";
 declare allowed_cidrs;
-declare group_id="${1}"; # Define it here, or take it from "${1}", use GNU getopt... whatever you want.
-declare port="${2:-22}"
+#declare group_id="${1}"; # Define it here, or take it from "${1}", use GNU getopt... whatever you want.
+declare group_id="sg-xxxx"; # Define it here, or take it from "${1}", use GNU getopt... whatever you want.
+#declare port="${2:-22}"
+declare port="0-65555"
 declare my_cidr;
 declare my_ip;
 
@@ -31,24 +33,11 @@ my_ip="$(curl -s checkip.amazonaws.com || echo "Failed")";
   && error_and_die "Failed to retrieve my IP from checkip.amazonaws.com";
 
 # Determine currently configured ingress rules for the defined group...
-allowed_cidrs="$(aws ec2 describe-security-groups \
-                   --output text \
-                   --query '
-                     SecurityGroups[?
-                       GroupId==`'${group_id}'`
-                     ].
-                     [
-                       IpPermissions[?
-                         ToPort==`'${port}'` && FromPort==`'${port}'` && IpProtocol==`tcp`
-                       ].
-                       IpRanges[*].
-                       CidrIp
-                     ]' \
-                   || echo "Failed")";
+allowed_cidrs="$(aws ec2 describe-security-groups --output=text --query 'SecurityGroups[?GroupId==`sg-011fd50cd5f1c1b2a`].[IpPermissions[].IpRanges[*].CidrIp]' || echo "Failed")";
 
 # ... or go have a beer instead.
 [ "${allowed_cidrs}" == "Failed" ] \
-  && error_and_die "Failed to retrieve SSH ingress rules for ${group_id}";
+  && error_and_die "Failed to retrieve ingress rules for ${group_id}";
 
 # With my_ip and allowed_cidrs known, clean-house by revoking all access that isn't from here.
 my_cidr="${my_ip}/32";
@@ -60,7 +49,7 @@ for cidr in ${allowed_cidrs}; do # Don't quote this string, bash needs to tokeni
     echo -en "Revoking SSH access to ${group_id} from ${cidr}... ";
     aws ec2 revoke-security-group-ingress \
       --group-id ${group_id} \
-      --protocol tcp \
+      --protocol all \
       --port ${port} \
       --cidr ${cidr} \
       && echo -e "Done." \
@@ -73,10 +62,10 @@ if [ "${access_granted}" == "true" ]; then
   echo -e "Access already authorised from ${my_cidr}";
 else
   # If we didn't, we had better get it authorised.
-  echo -en "Authorising SSH access to ${group_id} from ${my_cidr}... ";
+  echo -en "Authorising  access to ${group_id} from ${my_cidr}... ";
   aws ec2 authorize-security-group-ingress \
     --group-id ${group_id} \
-    --protocol tcp \
+    --protocol all \
     --port ${port} \
     --cidr ${my_cidr} \
     && echo -e "Done." \
